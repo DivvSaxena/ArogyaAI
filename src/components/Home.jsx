c371625ce63fbb0ec971d7d57b8e7d12b2f54a57
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2024-09-27 15:30:10 +0530

    changes added

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        Arogya Ai
+    </div>
+  )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import './index.css'
+import Home from './components/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/ai-app" element={<App />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+  </StrictMode>
+  ,
 )
